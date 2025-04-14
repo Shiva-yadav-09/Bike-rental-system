2ac836474861a45a448a911eb4e15c7dc40c62a9
Shiva-yadav-09 <shivathonda37@gmail.com>
2025-04-14 07:34:06 +0530

    Add files via upload

---
+++ Insert_customer.sql
@@ -0,0 +1,7 @@
+INSERT INTO Customer (FirstName, LastName, Email, Phone, LicenseNumber, MembershipStatus)  
+VALUES  
+('John', 'Doe', 'john.doe@example.com', '555-1234', 'DL123456', 1),  
+('Jane', 'Smith', 'jane.smith@example.com', '555-5678', 'DL654321', 0),  
+('Mike', 'Johnson', 'mike.johnson@example.com', '555-9876', 'DL789012', 1),  
+('Emily', 'Davis', 'emily.davis@example.com', '555-4321', 'DL345678', 0),  
+('Robert', 'Brown', 'robert.brown@example.com', '555-8765', 'DL901234', 1);  
